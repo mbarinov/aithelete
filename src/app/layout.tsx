2496d59a09aa0a7fe125217d59ca0ee38a2e6a69
Max Barinov <me@maxbarinov.com>
2024-09-01 19:20:24 +0400

    Add Loado SDK

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export default async function RootLayout({
                 rel="icon"
                 href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏋️‍♀️</text></svg>"
             />
+            {process.env.NODE_ENV === 'production' && (
+                <script async src='https://cdn.loado.dev/sdk.js'></script>
+            )}
         </head>
         <body
             className="flex flex-col min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
